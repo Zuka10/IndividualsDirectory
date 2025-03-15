e4e5a0624bc5bb3d4cfc80db3376b1f2525bcf4f
Zura Gobronidze <zura_gobronidze@epam.com>
2025-03-15 13:28:09 +0400

    add AutoMapper to simplyfy process of mapping entities

---
+++ IndividualsDirectory.Application/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using IndividualsDirectory.Application.City;
+
+namespace IndividualsDirectory.Application;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Domain.Entities.City, CityDto>();
+        CreateMap<Domain.Entities.City, CityDto>().ReverseMap();
+    }
+}
